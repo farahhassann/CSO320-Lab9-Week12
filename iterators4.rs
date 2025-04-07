a7664bdd7a87857b41694a2c676d7f71f7b79b3c
farahhassann <0farahhassan0@gmail.com>
2025-04-07 09:58:55 +0400

    Add files via upload

---
+++ iterators4.rs
@@ -0,0 +1,32 @@
+fn factorial(num: u64) -> u64 {
+    (1..=num).product()
+}
+
+fn main() {
+    println!("5! = {}", factorial(5)); // prints 120
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(factorial(4), 24);
+    }
+}
